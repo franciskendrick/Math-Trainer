7e104d641276470e9026595196a0aa2e7bd96439
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2023-06-21 14:02:23 +0800

    Added game module

---
+++ src/game/game.py
@@ -0,0 +1,33 @@
+from window import window
+import pygame
+import os
+
+pygame.init()
+resources_path = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), 
+        "..", "..", "resources", "game"
+        )
+    )
+
+
+class Game:
+    display_size_divider = 5
+
+    def __init__(self):
+        wd, ht = window.rect.size
+        self.display = pygame.Surface(
+            (wd // self.display_size_divider,
+            ht // self.display_size_divider),
+            pygame.SRCALPHA)
+        
+    def draw(self, display):
+        # Fill background
+        self.display.fill((235, 237, 233))
+
+        # Draw elements
+
+        # Blit menu's display to original display
+        resized_menu_display = pygame.transform.scale(
+            self.display, display.get_size())
+        display.blit(resized_menu_display, (0, 0))
